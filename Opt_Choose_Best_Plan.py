cd62b9e8f3e0b635ccc657fd1a3fff664b46e103
AChengy <alex.chengelis@gmail.com>
2016-05-11 18:23:13 -0400

    Updated Files

--- Opt_Choose_Best_Plan.py
+++ Opt_Choose_Best_Plan.py
@@ -5,51 +5,78 @@ class Opt_Choose_Best_Plan:
     def __init__(self, steps):
         self.executionSteps = steps
         self.QueryPlan = QueryExecution()
-        self.tables = [table('t1',1000), table('t2', 500), table('t3',2000)]
+        self.tables = [table('t1',1000,204, 20), table('t2', 500,102,20), table('t3',2000,40,100)]
+        self.correlated = False
 
         #go through the steps
         for step in steps:
             for i, part in enumerate(step):
+                if part == "Correlated":
+                    self.correlated= True
                 if part == "Join":
-                    AllJoinOptions = self.testAllJoins( step[i+1] , step[i+2])
-                    minVal = min(AllJoinOptions, key=AllJoinOptions.get)
-                    self.QueryPlan.addExecutionStep(minVal)
-                    self.QueryPlan.addCost(AllJoinOptions[minVal])
-                    print "Query Plan: "  , self.QueryPlan.totalCost
+                    if self.correlated:
+                        self.QueryPlan.addCost(self.QueryPlan.totalCost * self.TNJ(step[i+1], step[i+2], step[i+3],step[i+4]))
 
-                    #Setup the table with the new Values and add to the list of Tables
-                    selectivity = float(step[len(step)-2])
-                    #print "Selectivity: ", selectivity
 
-                    NewTableName = step[len(step)-1]
-                    #print "New Table Name: ", NewTableName
+                    else:
+                        AllJoinOptions = self.testAllJoins( step[i+1] , step[i+2])
+                        minVal = min(AllJoinOptions, key=AllJoinOptions.get)
+                        self.QueryPlan.addExecutionStep(minVal)
+                        self.QueryPlan.addCost(AllJoinOptions[minVal])
+                        #print "Query Plan: "  , self.QueryPlan.totalCost
 
-                    NewTableSize = self.getTableSize(step[1]) * selectivity
-                    #print "New Table Size: ", NewTableSize
+                        #Setup the table with the new Values and add to the list of Tables
+                        selectivity = float(step[len(step)-2])
+                        # print "Selectivity: ", selectivity
 
-                    self.tables.append(table(NewTableName, NewTableSize))
-                    print "Test All Joins: ", AllJoinOptions
-                    print "MinVal: ", minVal
+                        NewTableName = step[len(step)-1]
+                       # print "New Table Name: ", NewTableName
+
+                        NewTableSize = self.getTableSize(step[1]) * self.getTableSize(step[2]) * selectivity
+                        #print "New Table Size: ", NewTableSize
+
+                        self.tables.append(table(NewTableName, NewTableSize, None, None))
+                        #set table to sorted
+                        if minVal == "SMJL" or minVal == "SMJM":
+                            self.changeSorted(NewTableName)
+                        # print "Test All Joins: ", AllJoinOptions
+                        # print "MinVal: ", minVal
                 elif part == "project":
                     self.QueryPlan.addCost(self.projection(step[i+1], step[i+2]))
-                    print "Query Plan: "  , self.QueryPlan.totalCost
+                    #print "Query Plan: "  , self.QueryPlan.totalCost
                 elif part == "GroupBy" or part == "Aggregate":
                     self.QueryPlan.addCost(self.Grouper(step))
-                    print "Query Plan: " , self.QueryPlan.totalCost
+                    # print "Query Plan: " , self.QueryPlan.totalCost
+
+    def changeSorted(self, tableToChange):
+        for table in self.tables:
+            if table.name == tableToChange:
+                table.sorted == True
+
+    def getSorted(self, tableToCheck):
+        for table in self.tables:
+            if table.name == tableToCheck:
+                return table.sorted
+
 
     def Grouper(self, listSteps):
-        print " Grouper - list Steps: ", listSteps
+        #print " Grouper - list Steps: ", listSteps
 
         if 'without' in listSteps:
-            print "I AM HERE. "
-            print "List Steps 1: ", listSteps[1]
-            print "Last table: ", self.tables[len(self.tables)-1].name
+            # print "I AM HERE. "
+            # print "List Steps 1: ", listSteps[1]
+            # print "Last table: ", self.tables[len(self.tables)-1].name
             return self.getTableSize(self.tables[len(self.tables)-1].name)
         else:
-            print "List Steps 1: ", listSteps[1]
+            #print "List Steps 1: ", listSteps[1]
             size = self.getTableSize(listSteps[1])
-            self.tables.append(table(listSteps[len(listSteps)-1], size * float(listSteps[len(listSteps)-2])))
-            return size + size * math.log(size,2)  #for scan and a sort
+            self.tables.append(table(listSteps[len(listSteps)-1], size * float(listSteps[len(listSteps)-2]), None, None))
+            if not listSteps[3] == "None":
+                return 2*(self.getTableSize(listSteps[1])+self.getTableSize(listSteps[3]))  #for scan and a sort
+            else:
+                return 2*(self.getTableSize(listSteps[1]))
+
+
 
     def getTableSize(self,tableName):
         #for table in self.tables:
@@ -59,13 +86,19 @@ class Opt_Choose_Best_Plan:
                 return table.tableSize
 
     def projection(self,CurrTable, newTable):
+       # print "Curr Table: ", CurrTable
+
         tableSize = float(self.getTableSize(CurrTable))
        # print "newtable: ", newTable
-        newTableSize = tableSize * .20
+        newTableSize = tableSize
         #print "New Table Size: ", newTableSize
-        tempTable = table(newTable, newTableSize)
+        tempTable = table(newTable, newTableSize, None, None)
         self.tables.append(tempTable)
-        return tableSize + math.log10(tableSize)
+        if not self.getSorted(CurrTable):
+            #print "tableSize: ", tableSize
+            return 3*tableSize + math.log10(tableSize)
+        else:
+            return 2*tableSize
 
 
 
@@ -83,29 +116,52 @@ class Opt_Choose_Best_Plan:
 
         return Costs
 
+    #Method for finding the TNJ costs
+    def TNJ(self, left, right, selectivity, newTableName):
+        for table1 in self.tables:
+            if left == table1.name:
+                left = table1
+            if right == table1.name:
+                right = table1
+
+        #create the new table
+
+
+        newTupleSize = left.tupleSize + right.tupleSize
+        #print "newTupleSize:  ", newTupleSize
+        NewTableSize = float(selectivity) * left.tableSize * right.tableSize * (newTupleSize/4096.0)
+        #print "New Table Size: ", NewTableSize
+        #print "Left Table size: ", left.tableSize, " Right Table Size: ", right.tableSize
+        self.tables.append(table(newTableName, NewTableSize, (4096/newTupleSize), newTupleSize))
+
+
+        return left.tableSize + (left.numTuples*left.tableSize)*right.tableSize
+
+    #method for finding PNL Costs
     def PNL(self, temp1, temp2):
-        print "temp1 name: ", temp1
+        #print "temp1 name: ", temp1
         #get left Cost
         costLeft = self.getTableSize(temp1)
 
-        print "temp2 name: ", temp2
+        #print "temp2 name: ", temp2
         #get right Cost
         costRight = self.getTableSize(temp2)
 
-        print "Cost Left: ", costLeft
-        print "Cost Right: ", costRight
+        #print "Cost Left: ", costLeft
+        #print "Cost Right: ", costRight
         return min((costLeft + costLeft*costRight), (costRight + costRight*costLeft))
 
+    #Method for finding BNJM costs
     def BNJM(self, temp1, temp2):
         #print "temp1 name: ", temp1.name
 
         buffer = 50
 
-        print "temp1 name: ", temp1
+        #print "temp1 name: ", temp1
         #get left Cost
         costLeft = self.getTableSize(temp1)
 
-        print "temp2 name: ", temp2
+        #print "temp2 name: ", temp2
         #get right Cost
         costRight = self.getTableSize(temp2)
 
@@ -118,11 +174,11 @@ class Opt_Choose_Best_Plan:
 
         buffer = 50
 
-        print "temp1 name: ", temp1
+        #print "temp1 name: ", temp1
         #get left Cost
         costLeft = self.getTableSize(temp1)
 
-        print "temp2 name: ", temp2
+        #print "temp2 name: ", temp2
         #get right Cost
         costRight = self.getTableSize(temp2)
 
@@ -165,7 +221,7 @@ class Opt_Choose_Best_Plan:
 
         #print "Cost Left: ", costLeft
         #print "Cost Right: ", costRight
-        return 3*(costLeft + costRight)
+        return 2*3*(costLeft + costRight)
 
     def BNJL(self, temp1, temp2):
         #print "temp1 name: ", temp1.name
@@ -206,7 +262,7 @@ class Opt_Choose_Best_Plan:
 
 
 class table:
-    def __init__(self,name, size):
+    def __init__(self,name, size, numTuples, tupeSize):
         self.pageSize = 4096
         self.blockSize = 100
         # self.t1Size = 1000
@@ -214,10 +270,14 @@ class table:
         # self.t3Size = 2000
         self.tableSize = size
         self.name = name
+        self.sorted = False
+        self.numTuples = numTuples
+        self.tupleSize = tupeSize
+
 
 class QueryExecution:
     def __init__(self):
-        self.totalCost = 0
+        self.totalCost = 1
         self.steps = []
 
     def addCost(self, cost):
--- Project.py
+++ Project.py
@@ -21,7 +21,6 @@ with open("InputQ1.txt", "r") as reader:
             temp.append(word)
         executionStepsQ1.append(temp)
 
-print executionStepsQ1
 
 #Getthe Rewritten Optimized Query input
 executionStepsRQ1 = []
@@ -38,14 +37,27 @@ with open("InputRQ1.txt", "r") as reader:
             temp.append(word)
         executionStepsRQ1.append(temp)
 
-print executionStepsRQ1
 
 Q1 = op.Opt_Choose_Best_Plan(executionStepsQ1)
 RQ1 = op.Opt_Choose_Best_Plan(executionStepsRQ1)
 
 
-print "Total Cost Q1: ", Q1.QueryPlan.totalCost
-print "Execution Steps Q1: ", Q1.QueryPlan.steps
+Q1time = int(Q1.QueryPlan.totalCost * 12 / 1000)
+Q1Secs = Q1time % 60
+Q1time/= 60
+Q1Min = Q1time % 60
+Q1hours = Q1time % 24
+
+
+RQ1time = int(RQ1.QueryPlan.totalCost * 12 / 1000)
+RQ1Secs = RQ1time % 60
+RQ1time/= 60
+RQ1Min = RQ1time % 60
+RQ1hours = RQ1time % 24
+
+
+# print "Total Cost Q1: ", Q1.QueryPlan.totalCost
+# print "Execution Steps Q1: ", Q1.QueryPlan.steps
 
 iterator = 0
 print "----------Q1------------"
@@ -54,12 +66,16 @@ with open("InputQ1.txt", "r") as reader:
 
     for line in reader:
         if "Join" in line:
-            print line.rstrip('\n'),"    ", Q1.QueryPlan.steps[iterator]
-            iterator += 1
+            print line.rstrip('\n')
         else:
             print line.rstrip('\n')
+
+print "\n\nTotal IO Cost: ", Q1.QueryPlan.totalCost
+print "Total time in (hour:min:sec): ",Q1hours, ":",Q1Min, ":",Q1Secs
+print "-------------------------"
+
 iterator = 0
-print "----------Q1------------"
+print "\n\n----------RQ1------------"
 with open("InputRQ1.txt", "r") as reader:
 
     for line in reader:
@@ -69,8 +85,39 @@ with open("InputRQ1.txt", "r") as reader:
         else:
             print line.rstrip('\n')
 
+print "Total IO Cost: ", RQ1.QueryPlan.totalCost
+print "Total time in (hour:min:sec): ",RQ1hours, ":",RQ1Min, ":",RQ1Secs
+print "----------------------------"
 
+#print "difference: ", (Q1.QueryPlan.totalCost - RQ1.QueryPlan.totalCost)
 
+# print "Total Cost RQ1: ", RQ1.QueryPlan.totalCost
+# print "Execution Steps RQ1", RQ1.QueryPlan.steps
+print "\n\n----------------------BEST PLAN-----------------------"
+if Q1.QueryPlan.totalCost < RQ1.QueryPlan.totalCost:
+    print "----------Q1------------"
+    with open("InputQ1.txt", "r") as reader:
 
-print "Total Cost RQ1: ", RQ1.QueryPlan.totalCost
-print "Execution Steps RQ1", RQ1.QueryPlan.steps
+        for line in reader:
+            if "Join" in line:
+                print line.rstrip('\n')
+            else:
+                print line.rstrip('\n')
+
+    print "\n\nTotal IO Cost: ", Q1.QueryPlan.totalCost
+    print "Total time in (hour:min:sec): ",Q1hours, ":",Q1Min, ":",Q1Secs
+
+else:
+    iterator = 0
+    print "\n\n----------RQ1------------"
+    with open("InputRQ1.txt", "r") as reader:
+
+        for line in reader:
+            if "Join" in line:
+                print line.rstrip('\n'),"    ", RQ1.QueryPlan.steps[iterator]
+                iterator += 1
+            else:
+                print line.rstrip('\n')
+
+    print "Total IO Cost: ", RQ1.QueryPlan.totalCost
+    print "Total time in (hour:min:sec): ",RQ1hours, ":",RQ1Min, ":",RQ1Secs
